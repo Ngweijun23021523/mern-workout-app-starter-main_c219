afc5f39a25125e49b3b634ce7a4443d0850b64a9
Ngweijun23021523 <23021523@myrp.edu.sg>
2025-02-11 17:38:40 +0800

    c

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,17 +1,17 @@
 import { useAuthContext } from './useAuthContext'
-import { useRecipeContext } from './useRecipeContext'  
+import { useWorkoutsContext } from './useWorkoutsContext'
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext()
-  const { dispatch: dispatchRecipes } = useRecipeContext()  
+  const { dispatch: dispatchWorkouts } = useWorkoutsContext()
 
   const logout = () => {
 
     localStorage.removeItem('user')
 
+    
     dispatch({ type: 'LOGOUT' })
-
-    dispatchRecipes({ type: 'SET_RECIPE', payload: [] })  
+    dispatchWorkouts({ type: 'SET_WORKOUTS', payload: null })
   }
 
   return { logout }
